76cbcfd8a2eaf930ee2afed4732604fc46d3c599
Kimserey Lam <kimserey.lam@gmail.com>
2016-11-09 09:00:40 +0000

    missingmethdoexception

---
+++ FSharpCrash.Core/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpCrash.Core
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("FSharpCrash.Core")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("kimsereylam")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ FSharpCrash.Core/MyPage.fs
@@ -0,0 +1,31 @@
+﻿namespace FSharpCrash.Core
+
+open System
+open System.Threading
+open System.Threading.Tasks
+open Xamarin.Forms
+
+type MyPage() as self =
+    inherit ContentPage()
+
+    let button = Button(Text = "Click Me!",
+                        VerticalOptions = LayoutOptions.CenterAndExpand,
+                        HorizontalOptions = LayoutOptions.CenterAndExpand)
+    do
+        self.Title <- "My content page"
+        self.Content <- button
+
+type MainPage() as self =
+    inherit NavigationPage()
+
+    do 
+        self.PushAsync(new MyPage())
+        |> Async.AwaitTask
+        |> Async.StartImmediate
+
+
+type App() as self =
+    inherit Application()
+
+    do
+        self.MainPage <- new MainPage()
